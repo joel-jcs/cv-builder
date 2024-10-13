0cec2b7fe825736affe09a7e0a7ab83a4c78cfd9
joel-jcs <joeljcs@protonmail.com>
2024-10-13 12:08:27 -0400

    finished replacing all sections with component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -31,29 +31,25 @@ export function Resume() {
               },
             ]}
           />
-          <section className="resumeSection">
-            <h2 className="sectionTitle">Certifications</h2>
-            <hr></hr>
-            <div className="entry">
-              <div className="entryPrimaryInfo">
-                <h3 className="entryTitle">Certification Source or Entity</h3>
-                <h4 className="entrySubtitle">Certification Title</h4>
-              </div>
-              <div className="entryLocationDate">
-                <span className="location">Location</span>
-                <span className="datePeriod">Date Period</span>
-              </div>
-            </div>
-          </section>
-          <section className="resumeSection">
-            <h2 className="sectionTitle">Skills</h2>
-            <hr></hr>
-            <ul>
-              <li className="bulletPoint">Skill 1</li>
-              <li className="bulletPoint">Skill 2</li>
-              <li className="bulletPoint">Skill 3</li>
-            </ul>
-          </section>
+          <Section
+            title="Certifications"
+            entries={[
+              {
+                title: "Certification Source or Entity",
+                subtitle: "Certification Title",
+                location: "Location",
+                datePeriod: "Date Period",
+              },
+            ]}
+          />
+          <Section
+            title="Skills"
+            entries={[
+              {
+                hasBulletsSection: true,
+              },
+            ]}
+          />
         </main>
       </div>
     </>
