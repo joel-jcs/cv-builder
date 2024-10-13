07e6f49cf8ab21f35999b398e88d57cbb904314c
joel-jcs <joeljcs@protonmail.com>
2024-10-13 16:26:06 -0400

    initial modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,14 @@
+import "../styles/Modal.css";
+
+export default function Modal() {
+  //To-do: The purpose of this modal will be to add or delete an entry.
+  // No need to add or delete an entry because
+  return (
+    <div className="modal">
+      <div className="modalContent">
+        <h2>Modal</h2>
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
