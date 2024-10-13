a87e834eacff40b10911232a1d6cf92bf2d41a57
joel-jcs <joeljcs@protonmail.com>
2024-10-13 16:26:14 -0400

    to-do

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,8 @@ import "../styles/Modal.css";
 
 export default function Modal() {
   //To-do: The purpose of this modal will be to add or delete an entry.
-  // No need to add or delete an entry because
+  // Though the project states to edit and submit entries, no need to do this
+  // given that my version works with direct input for editing
   return (
     <div className="modal">
       <div className="modalContent">
